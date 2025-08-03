640d69d8f82b1cb11c387a98eb0e24db9382c4d6
Divyanshu-DXS <DivyanshuSharmaDXS@gmail.com>
2025-08-03 18:49:27 -0400

    Added solution and approach for leet code challenge | Valid Parentheses

--- src/LeetCode/ValidParentheses.java
+++ src/LeetCode/ValidParentheses.java
@@ -34,3 +34,16 @@ public class ValidParentheses {
         System.out.println("For sequence : " + s + " Result = " + isValid(s));
     }
 }
+
+/* APPROACH TO SOLVING THIS : 
+    Initially tied without stack, but the complexity of nesting loops to match conditonis was getting complicated. 
+    Was suggested to see the Stacks approach. 
+    Since stacks follow last in first out approach
+    we can traverse the string from first element 
+    and if it has an openeing bracket i.e. (,{, or[ we can then push this into the stack 
+    else if the index has a closing bracket stored i.e. ),}, or}
+    we can then check if that matches with the bracket that is on the top of the stack and see if the pair matches
+    If the pair matches we have a match and we can pop that element of the stack out 
+    This shall reveal the next element (another open bracket stored earlier) and whose match can then be compared. 
+    if at the end of the loop the stack is empty that means all pairs matched and it was a vlid string else false. 
+ */
