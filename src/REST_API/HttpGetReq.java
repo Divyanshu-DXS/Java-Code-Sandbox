23263a61919a3642cf1715eacdd500e1c4343a93
Divyanshu-DXS <DivyanshuSharmaDXS@gmail.com>
2025-08-11 19:21:54 -0400

    Added GET request code this time for CAT FACT API

--- src/REST_API/HttpGetReq.java
+++ src/REST_API/HttpGetReq.java
@@ -5,6 +5,8 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class HttpGetReq {
 
@@ -41,6 +43,25 @@ public class HttpGetReq {
         System.out.println(response.statusCode());
         System.out.println(response.body());
 
+        // EXAMPLE 2 - CAT FACT API
+        HttpClient client1 = HttpClient.newHttpClient();
+        // When adding request headers, if the api does not support any other formats then it will only return response in the supported format
+        // for instance in vase of CAT FACT API it only supports JSON
+        // changing request header to application/xml or text/plain would not change the format of the output
+        HttpRequest request1 = HttpRequest.newBuilder()
+                                .uri(new URI("https://catfact.ninja/fact"))
+                                .headers("Accept","text/plain")
+                                .GET()
+                                .build();
+        HttpResponse<String> response1 = client1.send(request1, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response1.statusCode());
+        System.out.println(response1.body());
+        System.out.println(response1.headers());
+
+        // writing the response in a file
+        HttpResponse<Path> response2 = client1.send(request1, HttpResponse.BodyHandlers.ofFile(Paths.get("demofile.txt")));
+        // This overwrites a file if it is already present. if not it will create a new file and then write to it.
 
     }
 }
