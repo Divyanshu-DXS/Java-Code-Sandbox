ecdfa0953cebbf7d4aa2d369c5aa822c573d6437
Divyanshu-DXS <DivyanshuSharmaDXS@gmail.com>
2025-08-13 02:34:58 -0400

    Added a Http Delete request method

---
+++ src/REST_API/HttpDeleteReq.java
@@ -0,0 +1,21 @@
+package REST_API;
+
+import java.net.http.*;
+import java.net.URI;
+
+public class HttpDeleteReq {
+    public static void main(String[] args) throws Exception {
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://jsonplaceholder.typicode.com/posts/1"))
+                .DELETE()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Status Code: " + response.statusCode());
+        System.out.println("Response Body: " + response.body());
+    }
+}
+
