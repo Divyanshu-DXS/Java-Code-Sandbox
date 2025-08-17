a17fd4a61ded087870340ebfde984cfd8de5ad46
Divyanshu-DXS <DivyanshuSharmaDXS@gmail.com>
2025-08-17 14:30:34 -0400

    Added nested goruping example

--- src/Regex/RegexGrouping.java
+++ src/Regex/RegexGrouping.java
@@ -5,18 +5,45 @@ import java.util.regex.Pattern;
 
 public class RegexGrouping {
     public static void main(String[] args) {
-        String str = "abcccde abc abcabcabc acd bac abbsc abc";
-        Pattern pattern = Pattern.compile("(abc)+");
+        String str = "ab abb abbb ababab abababbbbb bac abc";
+        Pattern pattern = Pattern.compile("(ab)+");
+        // Without grouping if the pattern was instead syntaxed as ab+ it would have looked for matches like
+        // ab abb abbb abbbb and more, repeating only the value b not a, since not grouped
+        // demoed below
+        System.out.println("Grouoed (ab)+");
         Matcher matcher = pattern.matcher(str);
         while(matcher.find()){
             System.out.println(matcher.group());
         }
-        String date = "2025-08-15";
-        pattern =Pattern.compile("((\\d{4})-(\\d{2})-(\\d{2}))");
-        matcher=pattern.matcher(date);
+        System.out.println("Not grouped ab+");
+        pattern=Pattern.compile("ab+");
+        matcher = pattern.matcher(str);
         while(matcher.find()){
             System.out.println(matcher.group());
         }
-        System.out.println(matcher.groupCount());
+
+        // Date Example
+        String date = "2025-08-15";
+        pattern =Pattern.compile("(\\d{4})-(\\d{2})-(\\d{2})");
+        matcher=pattern.matcher(date);
+        while(matcher.find()){
+            System.out.println(matcher.group());
+            System.out.println("Year: " + matcher.group(1));
+            System.out.println("Month: " + matcher.group(2));
+            System.out.println("Day: " + matcher.group(3));
+        }
+        System.out.println("Number of groups in this pattern : "+matcher.groupCount());
+
+        // Nested grouping
+        pattern=Pattern.compile("((ab)(cd))+");
+        String newPat = "abcd asiofa abcd";
+        matcher=pattern.matcher(newPat);
+        while(matcher.find()){
+            System.out.println(matcher.group());
+            System.out.println(matcher.groupCount());
+            System.out.println(matcher.group(1));
+            System.out.println(matcher.group(2));
+            System.out.println(matcher.group(3));
+        }
     }
 }
