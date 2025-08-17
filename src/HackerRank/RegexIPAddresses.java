059b338e3f51063064a5238f8eb85c3a26d068d4
Divyanshu-DXS <DivyanshuSharmaDXS@gmail.com>
2025-08-17 14:04:03 -0400

    Added Regex Pattern explanation  IP address challenege code | HackerRank

--- src/HackerRank/RegexIPAddresses.java
+++ src/HackerRank/RegexIPAddresses.java
@@ -16,5 +16,16 @@ public class RegexIPAddresses {
     }
     class MyRegex{
         String pattern="((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
+        /*
+        25[0-5]          → matches 250–255
+        2[0-4][0-9]      → matches 200–249
+        [01]?[0-9][0-9]? → matches 0–199 with optional leading zeros
+
+        [01]?[0-9][0-9]? : explained below
+        [01]? → optional leading 0 or 1
+        [0-9] → mandatory digit
+        [0-9]? → optional second digit
+        This allows numbers: 0, 00, 01, 1, 10, …, 199
+         */
 
 }
