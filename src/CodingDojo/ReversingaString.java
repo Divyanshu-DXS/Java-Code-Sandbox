037bb7dad7ccfe73640eee9a33915924722b8bc7
Divyanshu-DXS <DivyanshuSharmaDXS@gmail.com>
2025-08-18 21:24:42 -0400

    Modifyung code for Coding Dojo Challenge- Reverse a String

--- src/CodingDojo/ReversingaString.java
+++ src/CodingDojo/ReversingaString.java
@@ -12,8 +12,16 @@ public class ReversingaString {
 
         stringReversal(str);
         System.out.println("By recurssion : " + stringReversalRecurssion(str));
-
+        System.out.println("True recursion reversal : "+stringRecursionTrueReversal(str));
+    
     }
+
+    /*
+     * The approach below works but is inefficient
+     * The iterations that happen in the for loop works on multiples String concatenations 
+     * That could lead to many temporary Strings -- > inefficient for larger inputs 
+     * The use of String Builder is encouraged in these scenarios 
+     */
     public static void stringReversal(String str){
         int len = str.length();
         String reversed="";
@@ -23,6 +31,15 @@ public class ReversingaString {
         }
         System.out.println("Your reversed String is : "+ reversed);
     }
+
+    /*
+     * This approach has been marked as not true recursion because : 
+     * True recursion should:
+     * 1. Break the problem into a smaller version.
+     * 2. Call itself with that smaller problem.
+     * 3. Use the return value to build the solution.
+     * Implemented true recursion below ..
+     */
     public static String stringReversalRecurssion(String str){
         int len = str.length();
         String reversed="";
@@ -33,5 +50,14 @@ public class ReversingaString {
         }
 
         return reversed;
+    }
+
+    public static String stringRecursionTrueReversal(String str){
+        if(str.isEmpty()){
+            return str;
+        }
+        // Recursive step: reverse the substring starting from index 1, 
+        // then append the first character at the end.
+        return stringRecursionTrueReversal(str.substring(1))+str.charAt(0);
     }
 }
