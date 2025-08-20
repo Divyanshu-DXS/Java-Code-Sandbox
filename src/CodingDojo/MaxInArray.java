da8513063e1631b990d9f65ce0099c7e5bdc0036
Divyanshu-DXS <DivyanshuSharmaDXS@gmail.com>
2025-08-20 15:36:58 -0400

    Adding code to find max number in an Array | normal apprach + recursive

---
+++ src/CodingDojo/MaxInArray.java
@@ -0,0 +1,40 @@
+package CodingDojo;
+
+import javax.naming.PartialResultException;
+import java.util.Scanner;
+
+public class MaxInArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size of the array");
+        int n = sc.nextInt();
+        int []arr = new int[n];
+        for(int i =0 ; i<n;i++){
+            System.out.print("Enter element #"+(i+1)+" : ");
+            arr[i]=sc.nextInt();
+        }
+        int maxValue = findMax(arr);
+        System.out.println("Max value in provided array is : "+ maxValue);
+        int maxValueRecursion = findMaxRecursion(arr,0);
+        System.out.println("Calculating max value using recursion : "+maxValueRecursion);
+    }
+
+    private static int findMax(int[] arr) {
+        if(arr==null || arr.length==0){
+            throw new IllegalArgumentException("Array provided is empty. Please try again.");
+        }
+        int max = arr[0];
+        for(int i : arr){
+            if(i>=max) max=i;
+        }
+        return max;
+    }
+
+    private static int findMaxRecursion(int[]arr, int index){
+        if(index==(arr.length-1)){
+            return arr[index];
+        }
+        int maxArrRemaining = findMaxRecursion(arr,index+1);
+        return Math.max(maxArrRemaining,arr[index]);
+    }
+}
