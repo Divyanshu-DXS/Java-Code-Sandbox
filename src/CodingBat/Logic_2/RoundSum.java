e801ef56299492dbeaf7c55c6dfbebbcdf7f6007
Divyanshu-DXS <DivyanshuSharmaDXS@gmail.com>
2025-08-19 20:06:50 -0400

    Coding Bat Challenge

---
+++ src/CodingBat/Logic_2/RoundSum.java
@@ -0,0 +1,18 @@
+package CodingBat.Logic_2;
+
+public class RoundSum {
+    public int roundSum(int a, int b, int c) {
+        return round10(a)+round10(b)+round10(c);
+        
+      }
+      
+      public int round10(int n){
+        if(n%10>=5){
+          return n+(10-n%10);
+        }
+        else
+        return n-n%10;
+      }
+      
+      
+}
