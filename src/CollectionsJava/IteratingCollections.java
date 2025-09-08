83f7e2e1704a9d96db977b52b3d86916a6ace13f
Divyanshu-DXS <DivyanshuSharmaDXS@gmail.com>
2025-09-08 19:12:26 -0400

    Cleaning code

--- src/CollectionsJava/IteratingCollections.java
+++ src/CollectionsJava/IteratingCollections.java
@@ -5,12 +5,27 @@ public class IteratingCollections {
         
         System.out.println("Different Iteration Techniques");
         List<String> names= new ArrayList<>(List.of("EMINEM", "DR DRE", "ICE CUBE","50 CENT", "SNOOP"));
+
+        // Unlike Arrays ArrayLists can be directly printed using sout
+        // That is because ArrayLists and other collections that implement collection framework override the toString() method
+        System.out.println("PRINTING DIRECTLY >>>");
+        System.out.println(names);
+        System.out.println();
+
         // Using for loop - the very basic approach
         System.out.println(">>> FOR LOOP >>>");
         for(int i=0;i<names.size();i++){
             System.out.println(names.get(i));
         }
         System.out.println(">>> FOR LOOP ENDS >>>");
+        System.out.println();
+
+        // Enhanced ForEach loop
+        System.out.println(">>> ENHANCED FOR EACH LOOP >>>");
+        for(String st:names){
+            System.out.println(st);
+        }
+        System.out.println(">>> ENHANCED FOR EACH LOOP ENDS >>>");
         System.out.println();
 
         // Using Iterator
@@ -23,58 +38,56 @@ public class IteratingCollections {
         System.out.println(">>> ITERATOR ENDS >>>");
         System.out.println();
 
-        // Unlike Arrays ArrayLists can be directly printed using sout
-        // That is because ArrayLists and other collections that implement collection framework override the toString() method
-        System.out.println("PRINTING DIRECTLY >>>");
+        // The importance of Iterators
+        // Iterators can also help us remove any values at the time of iteration as below :
+        Iterator<String> xx = names.iterator();
+        System.out.println("Removing an element from the arrayList - \"DR DRE\"");
+        while(xx.hasNext()){
+            if(xx.next().equals("DR DRE")){
+                System.out.println("Element has been removed");
+                xx.remove();
+              }
+          }
+        System.out.println("Checking the content of ArrayList after iteration and removal");
         System.out.println(names);
         System.out.println();
 
+        // If we were to try and remove an item while using enhanced foreach or a for loop it will throw an exception
+        // Throws java.util.ConcurrentModificationException
 
-        Iterator<String> xx = names.iterator();
-        while(xx.hasNext()){
-            // Iterators can also help us remove any values at the time of iteration as below :
-            if(xx.next().equals("DR DRE")){
-                xx.remove();
+        System.out.println("Testing what happens if we try to remove an item while using a for each loop");
+        try{
+        for(String st:names){
+            if(st.equals("EMINEM")){
+                names.remove(st);
+            }
+        }}
+        catch(Exception e){
+            e.printStackTrace();
         }
-    }
-    System.out.println("Checking the content of ArrayList after iteration and removal");
-    System.out.println(names);
+        // You can observe that the item has been removed but it ends up throwing an exception which in real time scenarios can be problematic
+        System.out.println(names);
+        // Adding back the elements deleted on the same indices
+        names.add(1,"DR DRE");
+        names.add(0,"EMINEM");
+        System.out.println(names);
+        System.out.println();
 
-    // Enhanced ForEach loop 
-    for(String st:names){
-        System.out.println(st);
-    }
-
-    // If we were to try and remove an item while using enhanced foreach it will throw an exception 
-    // Throws java.util.ConcurrentModificationException
-    try{
-    for(String st:names){
-        if(st.equals("EMINEM")){
-            names.remove(st);
+        System.out.println("Iterating over different Collections ... ");
+        // List (ArrayList, LinkedList, etc. )
+        List<String> ll = new ArrayList<String>(List.of("ABC", "DEF", "GHI", "JKL", "MNO")) ;
+        for (String string : ll) {
+            System.out.println(string);
         }
-    }}
-    catch(Exception e){
-        e.printStackTrace();
+        // Set (HashSet, TreeSet etc.)
+        Set<String> ss = new HashSet<>(Set.of("India", "Canada", "Russia", "Japan"));
+        for (String string : ss) {
+            System.out.println(string);
+        }
+        // Maps (needs entrySet())
+        Map<String, Integer> map = Map.of("A", 1, "B", 2);
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+        System.out.println(entry.getKey() + " => " + entry.getValue());
     }
-    System.out.println(names);
-    names.add("EMINEM");
-    System.out.println(names);
-
-    // Iterating over different Collections 
-    // List (ArrayList, LinkedList, etc. )
-    List<String> ll = new ArrayList<String>(List.of("ABC", "DEF", "GHI", "JKL", "MNO")) ;
-    for (String string : ll) {
-        System.out.println(string);
-    }
-    // Set (HashSet, TreeSet etc.)
-    Set<String> ss = new HashSet<>(Set.of("India", "Canada", "Russia", "Japan"));
-    for (String string : ss) {
-        System.out.println(string);
-    }
-    // Maps (needs entrySet())
-    Map<String, Integer> map = Map.of("A", 1, "B", 2);
-    for (Map.Entry<String, Integer> entry : map.entrySet()) {
-    System.out.println(entry.getKey() + " => " + entry.getValue());
-}
     }
 }
