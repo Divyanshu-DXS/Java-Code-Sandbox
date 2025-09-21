7ae5b30597881039ef19b52547df3bdad7346e6f
Divyanshu-DXS <DivyanshuSharmaDXS@gmail.com>
2025-09-21 15:18:08 -0400

    Added more tried out methods in the streams with lambdas

--- src/StreamsAPI/StreamsTestEx1.java
+++ src/StreamsAPI/StreamsTestEx1.java
@@ -1,5 +1,6 @@
 package StreamsAPI;
 
+import java.util.Comparator;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
@@ -18,8 +19,9 @@ public class StreamsTestEx1 {
 //        streamOfStrings.forEach(consumer);
 //        // This in result will give us a stream of infinite Strings -"Hello Streams".
 
-        // Using lambdas to shorten and work on this code above
-        Stream.generate(()->"Hello Stream!!").forEach(s-> System.out.println(s));
+//        // Using lambdas to shorten and work on this code above
+//        // THis code runs infinite loop, so keeping this commented out
+//        Stream.generate(()->"Hello Stream!!").forEach(s-> System.out.println(s));
 
         // the generate function provides us with an infinite stream, we also have a method 'of()' that provides us with a finite stream to work on
 
@@ -30,6 +32,31 @@ public class StreamsTestEx1 {
         // lets try to use lambda and then shorten this code above
         Stream.of(1,2,4,5,6,126,613,61,46,84,48,36,8,9464,2,784).forEach(i -> System.out.println(i));
 
+        System.out.println("*******************");
+
+        // trying to print out a stream of strings using lambdas and streams
+        Stream.of("Krsna","Divine","Karma","Kahlon","Dino","Em","Raftaar").forEach(s -> System.out.println(s));
+
+        System.out.println("*******************");
+
+        // Trying to use filter with it.
+        // Filter takes a predicate object (returns boolean) to operate and verify a statement that can help filter the stream.
+        Stream.of("Krsna","Divine","Karma","Kahlon","Dino","Em","Raftaar").filter(s -> s.length()>5).forEach(s -> System.out.println(s));
+
+        System.out.println("*******************");
+
+        // using count()
+        System.out.println(Stream.of("Krsna", "Divine", "Karma", "Kahlon", "Dino", "Em", "Raftaar").count());
+
+        System.out.println("*******************");
+
+        // findFirst()
+        System.out.println(Stream.of("Krsna", "Divine", "Karma", "Kahlon", "Dino", "Em", "Raftaar").findFirst());
+
+        System.out.println("*******************");
+
+        // Using sorted ()
+        Stream.of("Krsna", "Divine", "Karma", "Kahlon", "Dino", "Em", "Raftaar").sorted(Comparator.naturalOrder()).forEach(s -> System.out.println(s));
 
 
     }
