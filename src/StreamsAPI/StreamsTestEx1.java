13c5683fe69cce97181ebe50a2e4d23cc5a6b09a
Divyanshu-DXS <DivyanshuSharmaDXS@gmail.com>
2025-09-21 12:52:16 -0400

    Adding practice code for streams API - generate()

---
+++ src/StreamsAPI/StreamsTestEx1.java
@@ -0,0 +1,22 @@
+package StreamsAPI;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class StreamsTestEx1 {
+    public static void main(String[] args) {
+
+        Supplier<String> supplier = ()->"Hello Streams";
+        // Stream creates a pipeline that keeps generating data that needs to be operated upon - an infinite stream
+        // Stream object takes a supplier object as an argument that supplies the data
+        Stream<String> streamOfStrings = Stream.generate(supplier);
+
+        Consumer<String> consumer=s -> System.out.println(s);
+        // Using forEach here we are accepting the stream of data and keep asking the pipeline if there is any more data to offer.
+        // the forEach method here takes  a Consumer object as an argument, that consumes the data supplied by the Stream object.
+        streamOfStrings.forEach(consumer);
+        // This in result will give us a stream of infinite Strings -"Hello Streams".
+
+    }
+}
