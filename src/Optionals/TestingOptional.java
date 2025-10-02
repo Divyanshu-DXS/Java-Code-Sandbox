548f8bf7044cebcdd6bbfb492aef3a5768723dd6
Divyanshu-DXS <DivyanshuSharmaDXS@gmail.com>
2025-10-01 20:59:44 -0400

    updated optionals

--- src/Optionals/TestingOptional.java
+++ src/Optionals/TestingOptional.java
@@ -1,5 +1,6 @@
 package Optionals;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public class TestingOptional {
@@ -26,7 +27,30 @@ public class TestingOptional {
         System.out.println("Lets try and implement a toUpperCase on this : ");
         System.out.println(optionalStr.get().toUpperCase());
 
-        
+        System.out.println("********************");
+        System.out.println("CREATING OPTIONAL OBJECTS >>>> ");
+        // Creating option objects -- we can use either of the approaches below, in relation to what we might think the option would hols
+        // 1. empty() To create an empty Optional object
+        Optional<String> optEmpty = Optional.empty();
+        // 2. .of() we can also create an optional object using this >> but the value passed in this cannot be null
+        Optional<String> optOf = Optional.of("DEV");
+        // 3. ofNullable() in case we expect some null values
+        Optional<String> optOfNullable = Optional.ofNullable(null);
+        System.out.println(".empty() - " + optEmpty);
+        System.out.println(".of() - " + optOf);
+        System.out.println(".ofNullable() - " + optOfNullable);
+
+        System.out.println("********************");
+        System.out.println("Checking Values present : isEmpty() && isPresent()");
+        // isPresent() : we can check if there is a value in the optional object or not with the isPresent() method
+        System.out.println("case : isPresent ()");
+        System.out.println(optEmpty.isPresent()); // returns false
+        System.out.println(optOf.isPresent()); // returns true since it actually holds an object in it
+        // isEmpty() : is just the opposite of isPresent() as this checks if the optoinal is empty
+        System.out.println("case : isEmpty ()");
+        System.out.println(optEmpty.isEmpty()); // returns true
+        System.out.println(optOf.isEmpty()); // returns false since it actually holds an object in it
+
     }
 
     // The purpose of the class is to provide a type-level solution for representing optional values instead of null references.
