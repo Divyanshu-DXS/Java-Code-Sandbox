5310b06fd27db7502535d5126aaa94a0bdbe1837
Divyanshu-DXS <DivyanshuSharmaDXS@gmail.com>
2025-11-28 21:21:12 -0500

    added retry mechanism implementation - resilience4j

--- resilience4j/ServiceA/src/main/java/com/dxs/circuitBreaker/ServiceA/controller/ServiceAController.java
+++ resilience4j/ServiceA/src/main/java/com/dxs/circuitBreaker/ServiceA/controller/ServiceAController.java
@@ -1,11 +1,14 @@
 package com.dxs.circuitBreaker.ServiceA.controller;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/a")
@@ -17,9 +20,22 @@ public class ServiceAController {
     RestTemplate restTemplate;
 
     @GetMapping
-    @CircuitBreaker(name = "service-A")
+    @CircuitBreaker(name = "service-A",fallbackMethod = "serviceAFallback")
     public String serviceA(){
         return restTemplate.getForObject(baseUrl+"/b",String.class);
+    }
+
+    int count =1;
+    @GetMapping("/retryA")
+    @Retry(name = "retryService-A")
+    public String serviceARetryDemo(){
+        System.out.println("Retry method called "+ count++ +" @" + new Date());
+        return restTemplate.getForObject(baseUrl+"/b",String.class);
+    }
+
+    // make sure when creating the fallback method the return type should be the same, and we inject exception object
+    public String serviceAFallback(Exception ex){
+        return "Service A fallback method ! ...";
     }
 
 
