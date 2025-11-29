eef0484505eb855f698a2e9c853fb03998298180
Divyanshu-DXS <DivyanshuSharmaDXS@gmail.com>
2025-11-28 20:41:38 -0500

    added actuator config to the appliactoin.yml file

--- resilience4j/ServiceA/src/main/java/com/dxs/circuitBreaker/ServiceA/controller/ServiceAController.java
+++ resilience4j/ServiceA/src/main/java/com/dxs/circuitBreaker/ServiceA/controller/ServiceAController.java
@@ -1,5 +1,6 @@
 package com.dxs.circuitBreaker.ServiceA.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ public class ServiceAController {
     RestTemplate restTemplate;
 
     @GetMapping
+    @CircuitBreaker(name = "service-A")
     public String serviceA(){
         return restTemplate.getForObject(baseUrl+"/b",String.class);
     }
