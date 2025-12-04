42504bdb5aa5eda1038d35d906a47045d3359af2
Divyanshu-DXS <DivyanshuSharmaDXS@gmail.com>
2025-12-04 00:59:14 -0500

    added functionality to the demo proect

--- Spring_AOP/src/main/java/com/dxs/springBoot/AOP/AopDemo/aopConfig/AspectDemo.java
+++ Spring_AOP/src/main/java/com/dxs/springBoot/AOP/AopDemo/aopConfig/AspectDemo.java
@@ -12,12 +12,12 @@ import org.springframework.stereotype.Component;
 import java.time.Instant;
 
 @Aspect
-@Slf4j
+//@Slf4j
 @Component
 public class AspectDemo {
 
     Logger log = LoggerFactory.getLogger(AspectDemo.class);
-    @Pointcut("execution(* com.dxs.springBoot.AOP.AopDemo.controller.*.*(..))")
+    @Pointcut("execution(* com.dxs.springBoot.AOP.AopDemo.*.*.*(..))")
     public void pointcut1(){
 
     }
@@ -25,6 +25,34 @@ public class AspectDemo {
     @Before("pointcut1()")
     public void logBefore(JoinPoint jp){
         log.info("Entering the message block : "+ jp.getSignature() + " at : "+Instant.now());
+    }
+
+    @After("pointcut1()")
+    public void afterAdvice(JoinPoint jp){
+        log.info("Exiting the message block : "+ jp.getSignature() + " at : "+Instant.now() + "\nResult of the block was - "+jp.getTarget());
+    }
+
+    @AfterReturning(value = "pointcut1()", returning = "result")
+    public void afterReturningAdvice(Object result){
+        log.info("This is the after returning advice : the result is - " + result);
+    }
+
+    @AfterThrowing(value = "pointcut1()",throwing = "ex")
+    public void afterThrowingAdvice(Exception ex){
+        log.error("Exception occured :" + ex.getMessage());
+    }
+
+    @Around("pointcut1()")
+    public Object aroundAdvice(ProceedingJoinPoint pjp) throws Throwable{
+        long start = System.currentTimeMillis();
+
+        Object result = pjp.proceed();
+        log.info("This is the around block, capturing result of the block - " + result.toString());
+        long end = System.currentTimeMillis();
+        log.info("total time taken here for this block execution is " + (end-start));
+
+        return result;
+
     }
 
 }
