125d2e71db6bf5b5b22637a2d32b2442e4c15d71
Divyanshu-DXS <DivyanshuSharmaDXS@gmail.com>
2026-01-06 21:12:14 -0500

    Modified DockerFile - testinf WORKDIR by using BufferedWriter

--- Docker/docker/src/main/java/com/dxs/projects/demo/docker/controller/TestController.java
+++ Docker/docker/src/main/java/com/dxs/projects/demo/docker/controller/TestController.java
@@ -5,12 +5,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 @RestController
 @RequestMapping("demo-api")
 public class TestController {
 
+    BufferedWriter br;
+
+    {
+        try {
+            br = new BufferedWriter(new FileWriter("outputFile.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @GetMapping("/getMessage")
     public ResponseEntity<String> getMessage(){
+        String res = "Logged output : endpoint hit successfully.";
+        try {
+            br.write(res);
+            br.newLine();
+            br.flush();
+            br.close();
+            System.out.println("Wrote to the output log file");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return ResponseEntity.ok("Docker Demo File Reached");
     }
+
 }
