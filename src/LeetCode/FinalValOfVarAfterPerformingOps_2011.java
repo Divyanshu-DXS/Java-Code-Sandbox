af5ee2d061b8e3bc45a4eb3853aaa3135c97a3b6
Divyanshu-DXS <DivyanshuSharmaDXS@gmail.com>
2026-03-23 20:52:58 -0400

    Provided new solution with faster runtime

--- src/LeetCode/FinalValOfVarAfterPerformingOps_2011.java
+++ src/LeetCode/FinalValOfVarAfterPerformingOps_2011.java
@@ -1,5 +1,28 @@
 package LeetCode;
 
 public class  FinalValOfVarAfterPerformingOps_2011 {
-    
+    public int finalValueAfterOperations(String[] operations) {
+        int x=0;
+
+        for (String s : operations){
+            if(s.contains("++"))
+            x+=1;
+            else
+            if(s.contains("--"))
+            x-=1;
+        }
+        return x;
+    }
+
+    public int finalValueAfterOperations2(String[] operations) {
+        int ans=0;
+        for(int i=0;i<operations.length;i++){
+            if(operations[i].charAt(1)=='+'){
+                ans++;
+            }else{
+                ans--;
+            }
+        }
+        return ans;
+    }
 }
